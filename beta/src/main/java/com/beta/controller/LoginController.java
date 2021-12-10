22b8eea20dd5a4a64b65728836cd373354c6afb7
gaolingfeng <491550934@qq.com>
2021-12-10 20:33:13 +0800

    修改

--- beta/src/main/java/com/beta/controller/LoginController.java
+++ beta/src/main/java/com/beta/controller/LoginController.java
@@ -12,18 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
-@RequestMapping("login")
 public class LoginController {
 
     @GetMapping("/index")
     public Object index(String ssoToken) {
-//        JSONObject jsonObject = new JSONObject();
-//        jsonObject.put("name", "gao");
-//        String result = HttpUtil.sendPostRequest("http://localhost:5002/login/test", jsonObject);
-//
-//        JSONObject jsonObject1 = JSONObject.parseObject(result);
-//        return "你好! " + jsonObject1.getString("name");
-        log.info("姓名：" + SsoClientInterceptor.SSO_USER_TOKEN_INFO.get(ssoToken));
-        return "你好!" + SsoClientInterceptor.SSO_USER_TOKEN_INFO.get(ssoToken);
+//        log.info("姓名：" + SsoClientInterceptor.SSO_USER_TOKEN_INFO.get(ssoToken));
+//        return "你好!" + SsoClientInterceptor.SSO_USER_TOKEN_INFO.get(ssoToken);
+        return "index";
     }
 }
